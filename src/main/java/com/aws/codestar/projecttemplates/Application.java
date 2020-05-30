d43dddb22aa17124e98f5d9a0245674a0cd8c776
AlanClaudiaProject <aws.acproject@gmail.com>
2020-05-30 14:57:33 +0800

    log4j test
    log4j test

--- src/main/java/com/aws/codestar/projecttemplates/Application.java
+++ src/main/java/com/aws/codestar/projecttemplates/Application.java
@@ -1,5 +1,6 @@
 package com.aws.codestar.projecttemplates;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -14,7 +15,10 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
+    static Logger log = Logger.getLogger(Application.class.getName());
     public static void main(String[] args) {
+        log.debug("Hello this is a debug message");
+        log.info("Hello this is an info message");
         SpringApplication.run(Application.class, args);
     }
 
