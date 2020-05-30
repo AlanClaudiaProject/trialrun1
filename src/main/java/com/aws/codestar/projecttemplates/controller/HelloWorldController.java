2811f710b7f53f1ac7e440c75b37b9f81ef79feb
AlanClaudiaProject <aws.acproject@gmail.com>
2020-05-30 16:11:16 +0800

    test db
    test db

--- src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.aws.codestar.projecttemplates.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
@@ -17,15 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     private static final String MESSAGE_FORMAT = "Hello %s!";
-    static Logger logger = Logger.getLogger(HelloWorldController.class.getName());
+    private static Logger logger = Logger.getLogger(HelloWorldController.class);
 
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(path = "/test1", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity helloWorldGet(@RequestParam(value = "name", defaultValue = "World1") String name) {
         logServerInfo();
         return ResponseEntity.ok(createResponse(name));
     }
 
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    @RequestMapping(path = "/test2", method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity helloWorldPost(@RequestParam(value = "name", defaultValue = "World2") String name) {
         return ResponseEntity.ok(createResponse(name));
     }
@@ -35,9 +38,37 @@ public class HelloWorldController {
         return new JSONObject().put("Output", String.format(MESSAGE_FORMAT, name)).toString();
     }
 
-    private static void logServerInfo() {
+    private void logServerInfo() {
         logger.info("test");
-        System.out.println("test2"); //went to /opt/tomcat/logs/catalina.out
-
+        System.out.println("starting db connection");
+        try {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String dbName = System.getenv("RDS_DB_NAME");
+        String userName = System.getenv("RDS_USERNAME");
+        String password = System.getenv("RDS_PASSWORD");
+        String hostname = System.getenv("RDS_HOSTNAME");
+        String port = System.getenv("RDS_PORT");
+        String jdbcUrl = "jdbc:sqlserver://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
+        logger.trace("Getting remote connection with connection string from environment variables.");
+        System.out.println("dbName > "+dbName);
+        System.out.println("userName > "+userName);
+        System.out.println("password > "+password);
+        System.out.println("hostname > "+hostname);
+        System.out.println("port > "+port);
+        System.out.println("jdbcUrl > "+jdbcUrl);
+        System.out.println("connecting");
+        Connection con = DriverManager.getConnection(jdbcUrl);
+        System.out.println("connected");
+        logger.info("Remote connection successful.");
+        }
+        catch (ClassNotFoundException e) { 
+            System.out.println("class error");
+            logger.warn(e.toString());
+        }
+        catch (SQLException e) { 
+            System.out.println("sql error");
+            logger.warn(e.toString());
+        }
     }
+    
 }
